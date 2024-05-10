a11d0b7ac26e35239b95e2d5d83c5767ec600d79
calvindoesdata <calvinwraith@gmail.com>
2024-05-11 00:42:03 +0100

    removed commented out code

--- scraping/understat_matches.py
+++ scraping/understat_matches.py
@@ -32,14 +32,4 @@ for k,v in data.items():
             os.mkdir(os.path.join('data','understat',team,'2023','matches'))
     match_id.to_csv(f'data/understat/{team}/2023/matches/PL_matches.csv', index=False)
 
-# response = requests.get('https://understat.com/team/Newcastle_United/2023')
-
-# soup = BeautifulSoup(response.text, 'html.parser')
-# ugly_soup = str(soup)
-
-# match_id_data = re.search("var datesData .*= JSON.parse\('(.*)'\)", ugly_soup).group(1)
-# data = json.loads(match_id_data.encode('utf8').decode('unicode_escape'))
-
-# match_id = pd.json_normalize(data)
-# match_id.to_csv('data/understat/nufc_matches.csv', index=False)
 

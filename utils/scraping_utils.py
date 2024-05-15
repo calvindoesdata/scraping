6a9afd91839acb4a3fe55330781a7117eb244e0e
calvindoesdata <calvinwraith@gmail.com>
2024-05-15 11:52:52 +0100

    updated returns for proper scraping

--- utils/scraping_utils.py
+++ utils/scraping_utils.py
@@ -21,11 +21,18 @@ def get_matches(home_team, away_team):
 
     match = match_id_df[
         (match_id_df['h.title']==home_team) & (match_id_df['a.title']==away_team)]
+    
+    date = match['datetime'].values[0].split(' ')[0]
+    home_goals = int(match['goals.h'].values[0])
+    away_goals = int(match['goals.a'].values[0])
+    home_xg = float(match['xG.h'].values[0])
+    away_xg = float(match['xG.a'].values[0])
+    match_id = match['id'].values[0]
 
-    return match['id'].values[0]
+    return date, home_goals, away_goals, home_xg, away_xg, match_id
 
 def get_shot_data(home_team, away_team):
-    match_id = get_matches(home_team, away_team)
+    date, home_goals, away_goals, home_xg, away_xg, match_id = get_matches(home_team, away_team)
     response = requests.get(f'https://understat.com/match/{match_id}')
     soup = BeautifulSoup(response.text, 'html.parser')
     ugly_soup = str(soup)
@@ -37,6 +44,6 @@ def get_shot_data(home_team, away_team):
     home_shots = pd.DataFrame(data['h'])
     away_shots = pd.DataFrame(data['a'])
 
-    match_shots_df = pd.concat([home_shots,away_shots])
+    match_shots_df = pd.concat([home_shots,away_shots]).reset_index(drop=True)
 
-    return match_shots_df
+    return date, home_goals, away_goals, home_xg, away_xg, match_shots_df
